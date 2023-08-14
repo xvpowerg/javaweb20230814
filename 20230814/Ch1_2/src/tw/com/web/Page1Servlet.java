8be982374788bdcd34ce7bc861642dbd81c51b57
xvpowerg <c911010083@gmail.com>
2023-08-14 17:25:09 +0800

    Add files via upload

---
+++ 20230814/Ch1_2/src/tw/com/web/Page1Servlet.java
@@ -0,0 +1,19 @@
+package tw.com.web;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+
+@WebServlet("/page1")
+public class Page1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req,HttpServletResponse resp)throws  IOException{
+			//HttpServletRequest 接收輸入
+		    //HttpServletResponse 輸出
+		PrintWriter out = resp.getWriter();
+		out.print("Page1");
+		
+	}
+}
